3579358df232942e300b57f3e8696a8e36e40821
Nezreka <batmanesignal@gmail.com>
2023-01-23 16:03:05 -0800

    starting js from scratch, making it far more organized.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 
-let buildString = "";
-let value = "";
+let equation = "";
+let outputValue = "";
 let operator = "";
 const operations = ["+", "-", "/", "*"];
 const pie = 3.14159265359;
@@ -31,10 +31,6 @@ function operate(num1, num2, operation){
     return answer
 }
 
-function analyze(){
-    
-}
-
 function add(num1, num2){
     return num1 + num2
 }
@@ -51,76 +47,8 @@ function divide(num1, num2){
     return num1 / num2
 }
 
-function pi(){
-   
-}
 
-function decimal(){
-    if(value == ""){
-        value = "0";
-        buildString = "0";
-    }
-    buildString += ".";
-    value += ".";
-    setValue();
-}
 
-function forwardLook(){
-
-}
-
-function operatorPressed(operation){
-
-}
-   
-
-function numberPressed(number){
-
-}
-
-function setValueAndBuild(){
-    setValue();
-    setBuildString();
-}
-
-function setValue(){
-    if(value == ""){
-        document.getElementsByClassName("output")[0].textContent = 0
-    }else{
-        document.getElementsByClassName("output")[0].textContent = value;
-
-    }
-}
-
-function setBuildString(){
-    document.getElementsByClassName("previousInputs")[0].textContent = buildString
-}
-
-function clearOperator(){
-    
-}
-
-function clearBuildString(){
-    
-}
-
-function clearValue(){
-   value = ""
-   setValue()
-}
-
-function allClear(){
-    value = ""
-    buildString = ""
-    userNumbers = []
-    setValueAndBuild()
-}
-
-function clear(){
-    value = value.slice(0, -1)
-    buildString = buildString.slice(0, -1)
-    setValue()
-}
 
 function setButtons(){
     let buttons = document.querySelectorAll(".button");
@@ -137,22 +65,19 @@ function buttonClicked(button){
     
     switch(button.dataset.type){
         case "7": case "8": case "9": case "4": case "5": case "6": case "1": case "2": case "3": case "0":
-            numberPressed(button.dataset.type);
+            
             break;
         case "clearAll":
-            allClear();
+            
             break;
         case "clear":
-            clear()
             break;
         case operations[0]: case operations[1]: case operations[2]: case operations[3]:
-            operatorPressed(button.dataset.type)
+            
             break;
         case "=":
-            analyze();
             break;
         case ".":
-            decimal()
             break;
         case "pi":
             
